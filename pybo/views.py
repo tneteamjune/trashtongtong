6492dfa5fb1e6b66cd907cecf0443a0119c75426
jieun yang <tneteamjune@gmail.com>
2021-11-21 00:31:14 +0900

    211121_0018

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pybo/sun_work/index.html')
+#    return render(request, 'pybo/sun_work/index.html')
+    return render(request, 'pybo/sun_work/test.html')
 
 def test(request):
     return render(request, 'pybo/sun_work/test.html')
