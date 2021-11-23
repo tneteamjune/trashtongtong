89830c8515a698534213f3e0db4ac48797bd8507
jieun yang <tneteamjune@gmail.com>
2021-11-23 15:21:24 +0900

    folder organized_1123

--- pybo/views.py
+++ pybo/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pybo/sun_work/index.html')
+    return render(request, 'pybo/index.html')
  
 def notice(request):
-    return render(request, 'pybo/sun_work/notice.html')
+    return render(request, 'pybo/notice.html')
 
 def tip(request):
-    return render(request, 'pybo/sun_work/tip.jsp')
+    return render(request, 'pybo/tip.html')
 
 def mypage(request):
-    return render(request, 'pybo/sun_work/mypage.html')
+    return render(request, 'pybo/mypage.html')
 
 def greenpoint(request):
-    return render(request, 'pybo/sun_work/greenpoint.jsp')
+    return render(request, 'pybo/greenpoint.jsp')
