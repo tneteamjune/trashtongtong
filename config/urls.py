dad7e8bf22e03a1f23c842bad0cefb4bc027b1fa
jieun yang <tneteamjune@gmail.com>
2021-11-23 15:37:43 +0900

    211123_1537

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('tip/', views.tip, name='tip'),
     path('mypage/', views.mypage, name='mypage'),
     path('greenpoint/', views.greenpoint, name='greenpoint'),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
