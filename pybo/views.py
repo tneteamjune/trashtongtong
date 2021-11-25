4311562b947a05289b911274cc428f6896211e43
jieun yang <tneteamjune@gmail.com>
2021-11-25 10:29:48 +0900

    211125_1029

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question
+from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator  
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'pybo/index.html')
