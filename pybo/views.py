9f008e62343985981289dd11402205aaca6553a9
jieun yang <tneteamjune@gmail.com>
2021-11-25 09:40:51 +0900

    211125_0940

--- pybo/views.py
+++ pybo/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'pybo/index.html')
  
-def notice(request):
-    return render(request, 'pybo/notice.html')
+# def notice(request):
+#     return render(request, 'pybo/notice.html')
 
 def tip(request):
     return render(request, 'pybo/tip.html')
@@ -14,3 +14,66 @@ def mypage(request):
 
 def greenpoint(request):
     return render(request, 'pybo/greenpoint.html')
+
+def notice(request):
+    """
+    pybo 목록 출력
+    """
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
+    question_list = Question.objects.order_by('-create_date')
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)  
+
+@login_required(login_url='common:login')
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:notice') #수정
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
