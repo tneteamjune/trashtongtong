46ed61e3bdfeda196b5d097bdae4e3dd4572ebba
tneteamjune <87072265+tneteamjune@users.noreply.github.com>
2021-12-01 21:09:39 +0900

    211201_2104_menu_link

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,7 @@ from django.urls import path, include
 from pybo import views
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.auth import views as auth_views
-from common import views
+
 
 urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
@@ -18,9 +17,6 @@ urlpatterns = [
     path('tip/', views.tip, name='tip'),
     path('mypage/', views.mypage, name='mypage'),
     path('greenpoint/', views.greenpoint, name='greenpoint'),
-    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', common.views.signup, name='signup'),
     ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
