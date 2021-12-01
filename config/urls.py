e5588c77446b4f5c494d901edc0b13308fe8f5dc
tneteamjune <87072265+tneteamjune@users.noreply.github.com>
2021-12-01 21:04:15 +0900

    211201_2104_menu_link

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,9 @@ urlpatterns = [
     path('tip/', views.tip, name='tip'),
     path('mypage/', views.mypage, name='mypage'),
     path('greenpoint/', views.greenpoint, name='greenpoint'),
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
     ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
